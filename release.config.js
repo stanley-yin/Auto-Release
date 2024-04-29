35925f10afd5c12de74005e00e21b418189e2256
stanley-yin <stanley6140992@gmail.com>
2024-04-29 10:54:51 +0800

    feat: add plugin

--- release.config.js
+++ release.config.js
@@ -3,8 +3,9 @@ const config = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        "@semantic-release/changelog",
         ["@semantic-release/git", {
-            "assets": ["dist/*.js", "dist/*.js.map"],
+            "assets": ['CHANGELOG.md', 'package.json',],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github'
