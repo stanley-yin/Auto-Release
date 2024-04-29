cc5080b17324ab3aed39ffb31835d653a37a8d93
stanley-yin <stanley6140992@gmail.com>
2024-04-29 11:05:13 +0800

    feat: add changeglog.md

--- release.config.js
+++ release.config.js
@@ -5,7 +5,7 @@ const config = {
         '@semantic-release/release-notes-generator',
         "@semantic-release/changelog",
         ["@semantic-release/git", {
-            "assets": ['package.json',],
+            "assets": ['CHANGELOG.md', 'package.json',],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github'
