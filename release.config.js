31cf0bc7ef478432b2eca2718e6a3540f9db1c5c
stanley-yin <stanley6140992@gmail.com>
2024-04-29 11:00:41 +0800

    feat: remove commit to changelod

--- release.config.js
+++ release.config.js
@@ -5,7 +5,7 @@ const config = {
         '@semantic-release/release-notes-generator',
         "@semantic-release/changelog",
         ["@semantic-release/git", {
-            "assets": ['CHANGELOG.md', 'package.json',],
+            "assets": ['package.json',],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github'
